fffe29eb5082d855c20844b5760bc94796d87dc8
Coti Garcia <javiera.garcialatorre@gmail.com>
2021-02-07 13:28:09 -0500

    adding files

---
+++ src/components/FooterMain.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="footer-area-wrapper" :class="FooterStyle">
+    <div class="footer-area section-space--ptb_90">
+      <div class="container">
+        <div class="row footer-widget-wrapper">
+          <div class="col-lg-4 col-md-6 footer-widget">
+            <ul class="footer-widget__list">
+              <li>
+                Av. Santa María 2670, oficina 206 <br />Providencia, Santiago •
+                Chile
+              </li>
+              <li>
+                <a
+                  href="mailto:UCDDchile@UCDD.edu"
+                  class="hover-style-link"
+                  >UCDDchile@UCDD.edu</a
+                >
+              </li>
+              <li>
+                <a
+                  href="https://chile.UCDD.edu/en"
+                  target="_blank"
+                  class="hover-style-link"
+                  >chile.UCDD.edu</a
+                >
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-2 col-md-6 footer-widget">
+            <ul class="footer-widget__list">
+              <li>
+                <router-link
+                  :to="{ name: 'UCDDEEEUU' }"
+                  class="hover-style-link"
+                  >UCDD EE.UU</router-link
+                >
+              </li>
+              <li>
+                <router-link
+                  :to="{ name: 'RelacionHistorica' }"
+                  class="hover-style-link"
+                  >Relación histórica</router-link
+                >
+              </li>
+              <li>
+                <router-link
+                  :to="{ name: 'UCDDChile' }"
+                  class="hover-style-link"
+                  >UCDD Chile</router-link
+                >
+              </li>
+              <li>
+                <router-link :to="{ name: 'Mision' }" class="hover-style-link"
+                  >Nuestra misión</router-link
+                >
+              </li>
+              <li>
+                <router-link
+                  :to="{ name: 'QuienesSomos' }"
+                  class="hover-style-link"
+                  >Quiénes somos</router-link
+                >
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-3 col-md-6 footer-widget">
+            <ul class="footer-widget__list">
+              <li>
+                <router-link :to="{ name: 'Impacto' }" class="hover-style-link"
+                  >Impacto</router-link
+                >
+              </li>
+              <li>
+                <router-link
+                  :to="{ name: 'AreasTrabajo' }"
+                  class="hover-style-link"
+                  >Áreas de trabajo</router-link
+                >
+              </li>
+              <li>
+                <router-link
+                  :to="{ name: 'AporteSimetrico' }"
+                  class="hover-style-link"
+                  >Aporte simétrico</router-link
+                >
+              </li>
+              <li>
+                <router-link :to="{ name: 'Cifras' }" class="hover-style-link"
+                  >Cifras</router-link
+                >
+              </li>
+              <li>
+                <router-link
+                  :to="{ name: 'Proyectos' }"
+                  class="hover-style-link"
+                  >Principales proyectos</router-link
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright-area border-top section-space--ptb_30">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6 text-center text-md-left">
+            <span class="copyright-text"> © All rights reserved.</span>
+          </div>
+     
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterMain",
+  props: ["FooterStyle"],
+};
+</script>
+<style lang="scss" scoped>
+.logo-dark {
+  max-width: 250px;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="main-container">
+    <!-- Navbar section -->
+    <HeaderShowcase />
+
+    <!-- Intro -->
+    <HeroAppShowcase />
+
+    <!-- 1. Presentacion -->
+    <Presentacion />
+    <Cartas />
+
+    <!-- 2.	UCDD EE. UU. -->
+    <UCDDUS />
+
+    <!-- 3. three step process -->
+    <RelacionHistorica />
+
+    <!-- 4. UCDD Chile -->
+    <UCDDChile />
+
+    <!-- 5. Nuestra Mision -->
+    <NuestraMision />
+
+
+    <!-- 7. Impacto -->
+    <Impacto />
+
+    <!-- 8. Areas de trabajo -->
+    <AreasTrabajo />
+
+    <!-- 9. Aporte Simetrico -->
+    <AporteSimetrico />
+
+    <!--10. Cifras -->
+    <Cifras />
+
+    <!-- 11. Proyectos -->
+    <Proyectos />
+    <!-- overlay menu section -->
+    <OverlayMenu />
+
+    <FooterMain />
+  </div>
+</template>
+
+<script>
+import HeaderShowcase from "@/components/HeaderShowcase";
+import Cartas from "@/components/sections/Cartas";
+import HeroAppShowcase from "@/components/HeroAppShowcase";
+import UCDDUS from "@/components/sections/UCDDUS";
+import UCDDChile from "@/components/sections/UCDDChile";
+import Cifras from "@/components/sections/Cifras";
+import Presentacion from "@/components/sections/Presentacion";
+import RelacionHistorica from "@/components/sections/RelacionHistorica";
+import OverlayMenu from "@/components/OverlayMenu";
+import NuestraMision from "@/components/sections/NuestraMision";
+import AreasTrabajo from "@/components/sections/AreasTrabajo.vue";
+import Proyectos from "@/components/sections/Proyectos.vue";
+import Impacto from "@/components/sections/Impacto.vue";
+import AporteSimetrico from "@/components/sections/AporteSimetrico.vue";
+import FooterMain from "@/components/FooterMain";
+export default {
+  name: "app",
+  components: {
+    HeaderShowcase,
+    Cartas,
+    Cartas,
+    UCDDUS,
+    HeroAppShowcase,
+    Presentacion,
+    RelacionHistorica,
+    Cifras,
+    OverlayMenu,
+    NuestraMision,
+    AreasTrabajo,
+    Proyectos,
+    FooterMain,
+    UCDDChile,
+    Impacto,
+    AporteSimetrico,
+  },
+};
+</script>
+
